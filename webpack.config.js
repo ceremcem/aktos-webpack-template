c2597d61d2334df1e9f6f7f7575d2c3fc7c9b4ec
Cerem Cem ASLAN <ceremcem@ceremcem.net>
2016-01-05 21:35:57 +0200

    livescript support is added

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,13 @@
 module.exports = {
-	entry: "./entry.js",
+	entry: "./client/app.ls",
 	output: {
 		path: __dirname, 
 		filename: "./bundle.js"
+	},
+	module: {
+		loaders: [
+			{test: /\.ls$/, loader: 'livescript-loader'}
+		]
 	}
 }; 
 
